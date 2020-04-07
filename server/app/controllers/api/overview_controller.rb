5bb0030465e5abef8242c094ca513330f8cd5d6a
Tyler Iguchi <tyleriguchi@gmail.com>
2020-04-06 21:35:10 -0700

    use real data in api response

--- server/app/controllers/api/overview_controller.rb
+++ server/app/controllers/api/overview_controller.rb
@@ -2,6 +2,25 @@ class Api::OverviewController < Api::BaseController
   def index
     employees = CompanyDomain.get_employees_and_tasks
 
-    respond_success(employees)
+    serialized_employees = employees.map do |employee|
+      serialized_tasks = employee.tasks.map do |task|
+        {
+          id: task.id,
+          title: task.title,
+          description: task.description,
+          priority: task.priority,
+          state: task.state,
+          estimate: task.estimate
+        }
+      end
+
+      next {
+        id: employee.id,
+        name: employee.name,
+        tasks: serialized_tasks
+      }
+    end
+
+    respond_success(serialized_employees)
   end
 end
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,78 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Employee.create!(
+    name: "Delores Abernathy",
+    tasks: [
+      Task.create(
+        title: "Get cows",
+        description: "I need to find any lost cows",
+        priority: "critical",
+        state: "new",
+        estimate: "medium"
+      ),
+      Task.create(
+        title: "Get to center of the maze",
+        description: "I need to go through the maze to find the center",
+        priority: "medium",
+        state: "active",
+        estimate: "large"
+      )
+    ]
+)
+
+Employee.create!(
+  name: "Bernard Lowe",
+  tasks: [
+    Task.create(
+      title: "Debug glitch on website",
+      description: "Site flickers on certain monitors",
+      priority: "critical",
+      state: "resolved",
+      estimate: "large"
+    ),
+    Task.create(
+      title: "Organize papers",
+      description: "I need to organize my project papers",
+      priority: "low",
+      state: "new",
+      estimate: "small"
+    ),
+    Task.create(
+      title: "Fix glasses",
+      description: "They keep sliding down my face and need re-adjusting",
+      priority: "medium",
+      state: "active",
+      estimate: "large"
+    ),
+    Task.create(
+      title: "Inspect current builds",
+      description: "I need to run through the builds for any glaring bugs",
+      priority: "critical",
+      state: "active",
+      estimate: "large"
+    )
+  ]
+)
+
+Employee.create!(
+    name: "Charlotte Hale",
+    tasks: [
+      Task.create(
+        title: "Prepare for investor meeting",
+        description: "Need to do a presentation on new ideas",
+        priority: "critical",
+        state: "resolved",
+        estimate: "small"
+      ),
+      Task.create(
+        title: "Investigate signals",
+        description: "Strange signal coming from an unkown location",
+        priority: "medium",
+        state: "resolved",
+        estimate: "medium"
+      )
+    ]
+)

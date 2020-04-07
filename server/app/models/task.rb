17b39b829f57edc453df13b9df633f74af160b30
Tyler Iguchi <tyleriguchi@gmail.com>
2020-04-06 20:51:01 -0700

    add enumeriazations to task model

--- server/app/models/task.rb
+++ server/app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  extend Enumerize
+  enumerize :priority, in: [:critical, :medium, :low]
+  enumerize :state, in: [:new, :active, :resolved, :closed]
+  enumerize :estimate, in: [:small, :medium, :large]
 end
